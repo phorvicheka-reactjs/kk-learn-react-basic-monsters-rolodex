f8c5236a1cb9e54ed55ce0c851bc37e304a4d05e
Vicheka <phorvicheka@yahoo.com>
2020-09-15 18:44:16 +0900

    Updates

---
+++ precache-manifest.d085788a6aa148e75e35462f69cba27c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6938b377d0114fb633e2e6291566a2b0",
+    "url": "/react-basic-monsters-rolodex/index.html"
+  },
+  {
+    "revision": "315ab0b8dc909bab3d91",
+    "url": "/react-basic-monsters-rolodex/static/css/main.e238f30b.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/react-basic-monsters-rolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-basic-monsters-rolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "315ab0b8dc909bab3d91",
+    "url": "/react-basic-monsters-rolodex/static/js/main.0a53ab5f.chunk.js"
+  },
+  {
+    "revision": "f09159461e0d17a8117e",
+    "url": "/react-basic-monsters-rolodex/static/js/runtime-main.83fa02de.js"
+  }
+]);
